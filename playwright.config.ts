0c131e6d3e9c6c60383e7bca69b7d437dce57075
Hengki Setiawan <hengkisetiawan@example.com>
2026-07-28 18:18:47 +0800

    config: optimize Playwright projects for Chromium Desktop and Mobile Chrome Pixel 7

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,6 +52,5 @@ export default defineConfig({
   projects: [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
     { name: 'mobile-chrome', use: { ...devices['Pixel 7'] } },
-    { name: 'mobile-safari', use: { ...devices['iPhone 14'] } },
   ],
 });
